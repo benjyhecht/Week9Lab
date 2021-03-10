7037e137421b66835a9aebed0d0444ef3eb74899
829364 <829364@SAIT225171IT.ACDM.DS.SAIT.CA>
2021-03-10 09:23:48 -0700

    Week 8 Lab created web xml

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -8,16 +8,14 @@ package servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author 468181
+ * @author 829364
  */
-@WebServlet(name = "UserServlet", urlPatterns = {"/User"})
 public class UserServlet extends HttpServlet {
 
     /**
