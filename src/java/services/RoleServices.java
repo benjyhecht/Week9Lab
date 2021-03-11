306069c3b36f1a6162313e356cee7ce01bc052a8
468181 <468181@SAIT225790IT.ACDM.DS.SAIT.CA>
2021-03-11 11:01:31 -0700

    Updated servlet & RoleServices

--- src/java/services/RoleServices.java
+++ src/java/services/RoleServices.java
@@ -6,24 +6,35 @@
 package services;
 
 import java.util.List;
+import java.util.ArrayList;
 import models.Role;
 import dataaccess.RoleDB;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author 468181
  */
 public class RoleServices {
-    
+
     public RoleServices() {
-        
+
     }
-    
-    public List<String> getAllRoles(){
-        
+
+    public List<String> getAllRoles() {
+
         RoleDB roleDB = new RoleDB();
-        //return roleDB.getAll();
+        try {
+            List<Role> allRoles = roleDB.getAll();
+            ArrayList<String> roles = new ArrayList<String>();
+            for (Role r : allRoles) {
+                roles.add(r.getRole());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(RoleServices.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
-    
+
 }
--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -54,14 +54,14 @@ public class UserServlet extends HttpServlet {
                     selectedUser = us.getUser(email);
                     break;
                 case "add":
-                    if (us.addUser(email, firstName, lastName, password, active, Integer.parseInt(role))) {
+                    if (us.addUser(email, firstName, lastName, password, role, active)) {
                         message = "User added";
                     } else {
                         message = "Could not add new user";
                     }
                     break;
                 case "save":
-                    if (us.updateUser(email, firstName, lastName, password, active, Integer.parseInt(role))) {
+                    if (us.updateUser(email, firstName, lastName, password, role, active)) {
                         message = "User updated";
                     } else {
                         message = "Could not update user";
@@ -73,6 +73,7 @@ public class UserServlet extends HttpServlet {
                     } else {
                         message = "Could not delete user";
                     }
+                    break;
                 default:
                     break;
             }
