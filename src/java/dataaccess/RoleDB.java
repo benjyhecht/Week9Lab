71db48843ce652960177d6abdf3101a1b306c97f
DiegoWR <806509@SAIT225715IT.ACDM.DS.SAIT.CA>
2021-03-11 11:29:47 -0700

    Fixed geRole method

--- src/java/dataaccess/RoleDB.java
+++ src/java/dataaccess/RoleDB.java
@@ -20,79 +20,79 @@ import models.Role;
  * @author 468181
  */
 public class RoleDB {
+    private String querry = "";
+    PreparedStatement statement = null;
+    ResultSet result = null;
+    
     public List<Role> getAll() throws Exception
     {
         List<Role> roles = new ArrayList<>();
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        String sql = "SELECT * FROM role";
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+
+        querry = "SELECT * FROM role";
         
         try {
-            ps = con.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                int roleId = rs.getInt(1);
-                String userRole = rs.getString(2);
+            statement = connection.prepareStatement(querry);
+            result = statement.executeQuery();
+            while (result.next()) {
+                int roleId = result.getInt(1);
+                String userRole = result.getString(2);
                 Role allRoles = new Role(roleId, userRole);
                 roles.add(allRoles);
             }
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            DBUtil.closeResultSet(result);
+            DBUtil.closePreparedStatement(statement);
+            pool.freeConnection(connection);
         }
         return roles;
     }
     
     public int getRole (String roleName) {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
         
-        String sql = "SELECT role_id FROM role WHERE role_name=?";
+        querry = "SELECT role_id FROM role WHERE role_name=?";
         
         try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, roleName);
-            rs = ps.executeQuery();
+            statement = connection.prepareStatement(querry);
+            statement.setString(1, roleName);
+            result = statement.executeQuery();
+            result.next();
 
-            return rs.getInt(1);
+            return result.getInt(1);
             
         } catch (SQLException ex) {
             Logger.getLogger(RoleDB.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            DBUtil.closeResultSet(result);
+            DBUtil.closePreparedStatement(statement);
+            pool.freeConnection(connection);
         }
         return 0;
     }
     
     public String getRole (int role) {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
         
-        String sql = "SELECT role_id FROM role WHERE role_name=?";
+        String sql = "SELECT role_name FROM role WHERE role_id=?";
         
         try {
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, role);
-            rs = ps.executeQuery();
-
-            return rs.getString(1);
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, role);
+            result = statement.executeQuery();
+            result.next();
+ 
+            return result.getString(1);
             
         } catch (SQLException ex) {
             Logger.getLogger(RoleDB.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            DBUtil.closeResultSet(result);
+            DBUtil.closePreparedStatement(statement);
+            pool.freeConnection(connection);
         }
         return null;
     }
